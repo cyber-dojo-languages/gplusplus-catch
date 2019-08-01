fd6265a5641751afd91e1bdedb0b980a65b347d1
JonJagger <jon@jaggersoft.com>
2019-08-01 03:04:32 +0100

    compile catch.hpp with main() in Dockerfile, add .o file to image, use in makefile

--- start_point/hiker.tests.cpp
+++ start_point/hiker.tests.cpp
@@ -1,8 +1,8 @@
 // For more on how to use the excellent Catch test framework see
-// https://github.com/philsquared/Catch
+// https://github.com/catchorg/Catch2
 
 #include "hiker.hpp"
-#include "catch.hpp"
+#include <catch2/catch.hpp>
 
 TEST_CASE( "Life the universe and everything", "[hhgttg]" )
 {
